0a83c8fc9ce06473d80815d0c6fa42f438d04a4e
Fritz Von Scherzer <fritzmattheas@gmail.com>
2017-07-19 20:57:22 +0000

    Added the Cell and Utility Class. Now the application creates an array with the position of the mines (identified with a number 9). And returns the cell clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,69 @@
+/*eslint-disable no-param-reassign */
 class Game{
-  constructor(height, width){
-    this.row = height;
-    this.cells = width;
+  constructor(height = 9, width = 9, board){
+    this.rowSize = height;
+    this.cellsPerRow = width;
+    this.boardContainer = board;
+    this.cellQty = height*width;
   }
   
-  createRow(rowNumber, parent){
+  startGame(){
+    this.drawBoard();
+    this.createBoard();
+  }
+  
+  endGame(win){
+    if(win){
+      alert("Felicidades, ganaste!");
+    } else {
+      alert("Perdiste! :(")
+    }
+  }
+  
+  drawRow(rowNumber, parent){
     var row = document.createElement('div');
-    row.setAttribute('id','Row'+rowNumber);
+    row.setAttribute('id','Row'+(++rowNumber));
     row.setAttribute('class', 'row-container');
     parent.appendChild(row);
     return row;
   }
   
-  createCell(cellNumber, parent){
+  drawCell(cellNumber, rowNumber, parent){
     var cell = document.createElement('div');
     cell.setAttribute('class', 'cell-container');
+    cell.setAttribute('id','['+rowNumber+']['+cellNumber+']');
     parent.appendChild(cell);
   }
   
-  createBoard(parent){
-    for(let i=0; i<this.row; i++){
-      console.log(i + " rows");
-      var currentRow = this.createRow(i, parent);
-      console.log(currentRow)
-      
-      for(let j=0; j<this.cells; j++){
-        this.createCell(j, currentRow);
-      } // end of for j
-    } // end of for i
+  drawBoard(){
+    for(let i=0; i<this.rowSize; i++){
+      var currentRow = this.drawRow(i, this.boardContainer);
+      for(let j=0; j<this.cellsPerRow; j++){
+        this.drawCell(j+1, i+1, currentRow);
+      }
+    }
+
+  }  
+}
+class Cell {
+  static openCell(e){
+    let cellPos = e.target.id;
+    console.log(cellPos+' clicked. ');
   }
-  
+}
+
+class Utility {
+  static isMineDuplicated(mine, mineArr){
+    for(let m of mineArr){
+      if(m == mine){
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 var boardContainer = document.getElementById('board-container');
 // hardcoded for testing purposes
-var g = new Game(9,9);
-g.createBoard(boardContainer);
+const g = new Game(9,9, boardContainer);
+g.startGame();
